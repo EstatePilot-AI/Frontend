a42b14d33f4310eca6a5f5d45ea3f62bdd96b067
Khaled Mahran <khaled.mahran@ingame-sports.com>
2026-02-08 23:50:33 +0200

    Add authentication login and logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Toaster } from 'react-hot-toast'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/home/DashBoard'
+import Signin from './pages/auth/Signin'
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <>
+      <Toaster position="top-center" toastOptions={{ duration: 4000 }} />
+      <Routes>
+        <Route path='/' element={<Home />} />
+         <Route path='/login' element={<Signin />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ src/pages/home/components/Sidebar.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { FaSignOutAlt, FaBars } from 'react-icons/fa';
+import SidebarItems from './SidebarItems';
+import logo from '../../../assets/images/Container (1).png';
+import { LuLayoutDashboard } from "react-icons/lu";
+import { TbUsers } from "react-icons/tb";
+import { IoCallOutline } from "react-icons/io5";
+import { TbHeartRateMonitor } from "react-icons/tb";
+import { CiSettings } from 'react-icons/ci';
+import { logout, logoutApi } from '../../../redux/slices/authSlice';
+import { useSelector } from 'react-redux';
+
+const SideBar = () => {
+  const [isOpen, setIsOpen] = useState(true);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const token = useSelector((state) => state.auth.token);
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+    dispatch(logoutApi(token));
+    localStorage.removeItem('token');
+    navigate('/login');
+  };
+
+  return (
+    <div
+      className={`h-screen bg-white text-gray-200 flex flex-col p-5 shadow-lg transition-width duration-300 ${
+        isOpen ? 'w-64' : 'w-20'
+      }`}
+    >
+
+      {isOpen && (
+        <div className="flex items-center gap-3 mb-4">
+          <img src={logo} alt="Logo" className="w-10 h-10" />
+          <h1 className="text-lg font-bold text-gray-800">EstatePilot CRM</h1>
+        </div>
+      )}
+
+    
+      <button
+        onClick={toggleSidebar}
+        className="mb-6 text-gray-800 hover:text-gray-600 focus:outline-none"
+      >
+        <FaBars size={20} />
+      </button>
+
+
+      <SidebarItems icon={<LuLayoutDashboard />} label="Dashboard" isOpen={isOpen} />
+      <SidebarItems icon={<TbUsers />} label="Leads" isOpen={isOpen} />
+      <SidebarItems icon={<IoCallOutline />} label="Call Logs" isOpen={isOpen} />
+      <SidebarItems icon={<TbHeartRateMonitor />} label="Ai Monitoring" isOpen={isOpen} />
+      <SidebarItems icon={<CiSettings size={20}/>} label="Setting" isOpen={isOpen} />
+      <SidebarItems icon={<FaSignOutAlt />} label="Logout" isOpen={isOpen} onClick={handleLogout} />
+
+
+     
+
+
+    </div>
+  );
+};
+
+export default SideBar;
